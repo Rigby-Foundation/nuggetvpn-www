dccc66c31a715fc80810466c5d161b05372350ec
TurboRigby <100000848+TurboRigby@users.noreply.github.com>
2025-11-24 18:03:14 +0200

    Remove default adapter output options and add dynamic base path configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -12,14 +12,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html', // <-- ВАЖНО ДЛЯ GITHUB PAGES
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
